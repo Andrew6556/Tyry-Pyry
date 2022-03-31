b818a2eeb57b26c3caf1febcf02621d4dbdc5a54
Andrey Kozlov <andrew08552gmail.com>
2022-03-31 18:34:10 +0300

    the end programm

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-num = []
-
-
-
-for i in range(1,101):
-    if i % 3 == 0 and i % 5 == 0:
-        num.append("тыры-пыры")
-    elif i % 3 == 0:
-        num.append("тыры")
-    elif i % 5 == 0:
-        num.append("пыры")
-    else:
-        num.append(i)
-
-for n in num:
-    print(n)
+list(map(lambda x: print(x), ["тыры-пыры" if num % 3 == 0 and num % 5 == 0 else "пыры" if num % 5 == 0 else 'тыры' if num % 3 == 0 else num for num in range(1, 101)]))
